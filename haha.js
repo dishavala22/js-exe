494e94f02ed3b7c409b4ee95e7cf18c98ca2bb88
disha2292 <disha2292@icloud.com>
2024-02-02 08:31:27 +0530

    practicing

---
+++ haha.js
@@ -0,0 +1,63 @@
+// function read() {
+//     var x = 10;
+//     if (1 === 1) {
+//         let x = 15;
+//         console.log(x);
+//     }
+//     console.log(x);
+// }
+// read();
+
+
+// function example() {
+//     let myVar = 'Using let';
+//     console.log(myVar);
+
+//     if (true) {
+//         let myVar = 'Using let in a different scope';
+//         console.log(myVar);
+
+//         var anotherVar = 'Using var in a different scope';
+//     }
+
+//     console.log(myVar); // Output: Using let
+//     console.log(anotherVar); // Output: Using var in a different scope
+// }
+
+// example();
+
+// var myVar = "disha";
+// console.log(myVar);
+
+// function example() {
+//     var myVar = 'Using let';
+//     console.log(myVar);
+
+//     if (true) {
+//         var myVar = 'Using let in a different scope';
+//         console.log(myVar);
+
+//         var anotherVar = 'Using var in a different scope';
+//     }
+
+//     console.log(myVar); // Output: Using let
+//     console.log(anotherVar); // Output: Using var in a different scope
+// }
+
+// example();
+
+// let a, b;
+// console.log(a + b);
+
+// var a = null;
+// console.log(typeof(a));
+// console.log(a);
+
+
+// null == undefined // true
+// null === undefined // false
+
+
+// It means null is equal to undefined but not identical.
+
+// When we define a variable to undefined then we are trying to convey that the variable does not exist.When we define a variable to null then we are trying to convey that the variable is empty.
